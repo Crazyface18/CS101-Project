f95195be41477998b886f1c8e350ad6e4f1425fa
meenadeshpande12 <99935621+meenadeshpande12@users.noreply.github.com>
2022-02-18 20:09:43 -0600

    Add files via upload

---
+++ hamming_dist(dna1, dna2).py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[105]:
+
+
+# This function will return the hamming distance between 2 strands of nucleotides
+dna1 = ""
+dna2 = ""
+def hamming_dist(dna1,dna2):
+    count = 0
+    x = len(dna1)
+    for nucleotide in range(0,x):
+        if dna1[nucleotide] != dna2[nucleotide]:
+            count += 1
+    return count
+print(hamming_dist(dna1,dna2))
+
+
+# In[77]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
