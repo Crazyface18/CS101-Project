2a0597612a5365dfb644ab13026f43f044accaaa
meenadeshpande12 <99935621+meenadeshpande12@users.noreply.github.com>
2022-02-18 20:23:06 -0600

    Add files via upload

---
+++ s(dna).py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[57]:
+
+
+# s(dna) function
+# The purpose of this function is to count the number of nucleotides in a given strand and return a dictionary showing the counts for each nucleotide. 
+dna =''
+def s(dna):
+    countA = dna.count('A')
+    countC = dna.count('C')
+    countG = dna.count('G')
+    countT = dna.count('T')
+    s = {
+        'A': countA,
+        'C': countC, 
+        'G': countG, 
+        'T': countT
+    }
+    return(s)
+
+print(s(dna))
+
+
+# In[60]:
+
+
+#Test function 
+dna ='AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+def s(dna):
+    countA = dna.count('A')
+    countC = dna.count('C')
+    countG = dna.count('G')
+    countT = dna.count('T')
+    s = {
+        'A': countA,
+        'C': countC, 
+        'G': countG, 
+        'T': countT
+    }
+    return(s)
+print(s(dna))
+
+
+# In[ ]:
+
+
+
+
