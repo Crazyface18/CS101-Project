f3a8adf8e00b4c382a984cec79104acfc39428ee
zylle <99935614+zylleee@users.noreply.github.com>
2022-02-24 18:42:42 -0600

    Update milestone1.py

--- milestone1.py
+++ milestone1.py
@@ -13,3 +13,13 @@ def dna2rna(dna):
         elif symbol == "G":
             rna = rna + "G"
     return rna
+
+"""
+this function is supposed to take 6 nonnegative integers, which corresponds to
+a number of couples in a population
+each couple makes two offspring. this function will return how many offspring
+are created with the dominant phenotype.
+"""
+
+def count_dom_phenotype(genotypes):
+    return (genotypes[0]*2) + (genotypes[1]*2) + (genotypes[2]*2) + (genotypes[3]*1.5) + (genotypes[4]*1) + (genotypes[5]*0)
