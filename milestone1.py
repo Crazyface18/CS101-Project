dc21b13676e215d6b928fdb2dd5618ed4d79112f
meenadeshpande12 <99935621+meenadeshpande12@users.noreply.github.com>
2022-02-24 19:38:48 -0600

    Create milestone1.py

---
+++ milestone1.py
@@ -0,0 +1,26 @@
+# s(dna) function
+# The purpose of this function is to count the number of nucleotides in a given strand and return a dictionary showing the counts for each nucleotide. 
+dna =''
+def s(dna):
+    countA = dna.count('A')
+    countC = dna.count('C')
+    countG = dna.count('G')
+    countT = dna.count('T')
+    s = {
+        'A': countA,
+        'C': countC, 
+        'G': countG, 
+        'T': countT
+    }
+    return(s)
+# hamming_dist
+# This function will return the hamming distance between 2 strands of nucleotides
+dna1 = ""
+dna2 = ""
+def hamming_dist(dna1,dna2):
+    count = 0
+    x = len(dna1)
+    for nucleotide in range(0,x):
+        if dna1[nucleotide] != dna2[nucleotide]:
+            count += 1
+    return count
