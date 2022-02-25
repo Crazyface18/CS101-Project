ffb7b8fba07d80d47ac15c95013fc23be0c8b84b
zylle <99935614+zylleee@users.noreply.github.com>
2022-02-24 18:42:06 -0600

    Update milestone1.py

--- milestone1.py
+++ milestone1.py
@@ -1,1 +1,15 @@
+# this function is supposed to turn dna into rna strands
+# specifically turns "T" into "U" in the dna strands
 
+def dna2rna(dna):
+    rna = ""
+    for symbol in dna:
+        if symbol == "A":
+            rna = rna + "A"
+        elif symbol == "T":
+            rna = rna + "U"
+        elif symbol == "C":
+            rna = rna + "C"
+        elif symbol == "G":
+            rna = rna + "G"
+    return rna
