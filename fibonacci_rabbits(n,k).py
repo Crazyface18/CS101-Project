a5209337c1212773c4bf7e094b4b7490b8e475dc
meenadeshpande12 <99935621+meenadeshpande12@users.noreply.github.com>
2022-02-26 14:34:45 -0600

    Add files via upload

---
+++ fibonacci_rabbits(n,k).py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def fibonacci_rabbits(n,k):
+    if n == 1:
+        return (1)
+    elif n == 2:
+        return (1)
+    elif n >= 3:
+        return (fibonacci_rabbits(n-1,k) + k*fibonacci_rabbits(n-2,k))
+    else:
+        return (none)
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
