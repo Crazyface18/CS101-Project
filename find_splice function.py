529ba0e6bd67951be4e780ca435334f8c0df1ccc
zylle <99935614+zylleee@users.noreply.github.com>
2022-03-26 21:36:21 -0500

    Delete find_splice function.py

--- find_splice function.py
+++
@@ -1,9 +0,0 @@
-def find_splice(dna_motif, dna):
-    position_list = []
-    counter = 0 # for iterating through dna
-    while (counter < len(dna) and dna_motif): #checks through dna_motif & iterates through dna
-        if (dna_motif[0] == dna[counter]):
-            position_list.append(counter)
-            dna_motif = dna_motif[1:] #removes the first letter of motif
-        counter += 1
-    return position_list
