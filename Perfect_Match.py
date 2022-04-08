ba36313e8aeeb4ca3a8a00febaa188b8893f3d71
Crazyface18 <67551681+Crazyface18@users.noreply.github.com>
2022-04-08 14:12:57 -0500

    Update Perfect_Match.py

--- Perfect_Match.py
+++ Perfect_Match.py
@@ -4,39 +4,14 @@
 # In[28]:
 
 
-def perfect_match(rna):
+def perfect_match(rna): 
     GC=0
-    AU=0
-    from math import factorial
-    for i in rna:
-        if i == 'G':
+    AU=0 #defining the varriables that will be used.
+    from math import factorial #Total number of pairs that is present in a list can be found with the factorial of 1/2 the total number of nodes, so it is imported here.
+    for i in rna: #Because there are 2 sets of values that pair together, GC and AU, we basically have two different numbers of pairs. This for loop separates those two sets of pairs.
+        if i == 'G': #also, as the total perfect pairs is the factorial of 1/2 the total number of nodes, we only need to count one of the two pairing values. 
             GC+=1
         elif i == 'A':
             AU+=1
-    matches = factorial(GC)*factorial(AU)
+    matches = factorial(GC)*factorial(AU) #here we multiply the two sets of perfect pairs together, giving us the total number of perfect pairs.
     return (matches)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
